e6ed96c497acb842f441e496ab729d7ccb904f18
AlexD1533 <zeed1533@yandex.ru>
2025-12-21 19:25:10 +0300

    cs1

--- src/main/java/ru/yandex/practicum/filmorate/dao/dto/film/FilmDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/dto/film/FilmDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.dao.dto.mpa.MpaDto;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.MpaRating;
 
 import java.time.LocalDate;
 import java.util.HashSet;
