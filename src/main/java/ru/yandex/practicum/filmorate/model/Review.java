c2b19d68016cd09324ada61eadc2733b6d92d408
AlexD1533 <zeed1533@yandex.ru>
2026-01-16 21:04:31 +0300

    cs3

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -2,11 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
